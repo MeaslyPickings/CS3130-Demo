4abb63fd92bfe731ae3820578fe8513cbc16733c
MeaslyPickings <illogicaljp_211@hotmail.com>
2017-05-10 21:50:23 -0300

    This took me way to long to figure out

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,9 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        TextView textView = (TextView) findViewById(R.id.helloText);
+        textView.setText(message);
     }
 }
